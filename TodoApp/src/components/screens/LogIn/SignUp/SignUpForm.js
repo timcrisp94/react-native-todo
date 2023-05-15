882006e8c6a37c9b2a23c33a0f7d89e41f5633a6
Tim Crisp <timothycrisp@gmail.com>
2023-05-15 11:08:38 -0500

    edit fields for sign up form

--- TodoApp/src/components/screens/LogIn/SignUp/SignUpForm.js
+++ TodoApp/src/components/screens/LogIn/SignUp/SignUpForm.js
@@ -8,7 +8,13 @@ const SignUpForm = ({ onNewItem }) => {
   const handleSubmit = async () => {
     if (text) {
       try {
-        const response = await api.post('users/', { title: text })
+        const response = await api.post('users/', { 
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            user_name: user_name,
+            password: password
+        })
         setText('')
         onNewItem(response.data)
       } catch (error) {
@@ -20,13 +26,49 @@ const SignUpForm = ({ onNewItem }) => {
   return (
     <View className='m-10'>
       <Text className='text-center pb-3 text-xl'>
-        Add a new item to the list
+        First Name
       </Text>
       <TextInput
         className='h-10 text-xl bg-white rounded-md p-2 text-center'
         onChangeText={setText}
-        value={text}
-        placeholder="Enter new item"
+        value={first_name}
+        placeholder="First Name"
+      />
+      <Text className='text-center pb-3 text-xl'>
+        Last Name
+      </Text>
+      <TextInput
+        className='h-10 text-xl bg-white rounded-md p-2 text-center'
+        onChangeText={setText}
+        value={last_name}
+        placeholder="Last Name"
+      />
+      <Text className='text-center pb-3 text-xl'>
+        Email
+      </Text>
+      <TextInput
+        className='h-10 text-xl bg-white rounded-md p-2 text-center'
+        onChangeText={setText}
+        value={email}
+        placeholder="Email"
+      />
+      <Text className='text-center pb-3 text-xl'>
+        User Name
+      </Text>
+      <TextInput
+        className='h-10 text-xl bg-white rounded-md p-2 text-center'
+        onChangeText={setText}
+        value={user_name}
+        placeholder="User Name"
+      />
+      <Text className='text-center pb-3 text-xl'>
+        Password
+      </Text>
+      <TextInput
+        className='h-10 text-xl bg-white rounded-md p-2 text-center'
+        onChangeText={setText}
+        value={password}
+        placeholder="Password"
       />
       <View className='bg-green-100 rounded p-3 mx-10 my-3'>
         <Button title="Add" onPress={handleSubmit} />
